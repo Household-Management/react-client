7253461b61b0d797a7712af5383336a1c007256f
Damien Marble <damienmarble@gmail.com>
2018-09-23 01:41:03 -0700

    Implemented signing in.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
-import { Component } from 'react';
+import {Component} from 'react';
 import * as React from "react";
 import './App.css';
-import { Route } from "react-router-dom";
+import {Route} from "react-router-dom";
 import WelcomeView from "./views/WelcomeView";
+import LoginView from "./views/LoginView";
+import {Switch} from "react-router";
+import { Auth } from "aws-amplify";
+import AwsExports from "./aws-exports";
+
+Auth.configure(AwsExports);
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-          <Route path="/" component={WelcomeView}/>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <Switch>
+                    <Route path="/" component={WelcomeView} exact/>
+                    <Route path="/login" component={LoginView}/>
+                </Switch>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/views/LoginView.tsx
@@ -0,0 +1,110 @@
+import {Component} from "react";
+import {match, RouteComponentProps, StaticContext} from "react-router";
+import {History, Location} from "history";
+import Grid from "@material-ui/core/Grid";
+import * as React from "react";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import { Auth } from "aws-amplify";
+import WithRouter from "./WithRouter";
+
+@WithRouter
+export default class LoginView extends Component<LoginViewProps, LoginViewState> {
+    constructor(props: LoginViewProps) {
+        super(props);
+        this.state = {};
+        this.signIn = this.signIn.bind(this);
+    }
+
+    signIn() {
+        if(this.state.email && this.state.password) {
+            this.setState({
+               emailInputMessage: undefined,
+               passwordInputMessage: undefined
+            });
+            this.setState({
+                signInStatus: "Signing In..."
+            });
+            Auth.signIn(this.state.email!, this.state.password).then(()=>{
+                this.setState({
+                    signInStatus: undefined
+                });
+            }).catch((err)=>{
+                this.setState({
+                    signInStatus: undefined
+                });
+                console.error(err);
+                switch (err.code){
+                    case "UserNotFoundException":
+                        this.setState({
+                            emailInputMessage: "User was not found"
+                        });
+                        break;
+                    case "NotAuthorizedException":
+                        this.setState({
+                            passwordInputMessage: "Username and password were invalid"
+                        });
+                }
+            });
+        }
+    }
+
+    render() {
+        return (
+            <Grid
+                container
+                direction="row"
+                justify="center"
+                alignItems="center"
+                spacing={24}
+            >
+                <Grid item xs={12}>
+                    <div style={{textAlign: "center"}}>
+                        <TextField required label={this.state.emailInputMessage || "Email"} placeholder="Email"
+                                   error={!!this.state.emailInputMessage}
+                                   onChange={event => {
+                                       this.setState({
+                                           email: event.target.value,
+                                           lastUpdatedProperty: "email"
+                                       })
+                                   }}/>
+                    </div>
+                </Grid>
+                <Grid item xs={12}>
+                    <div style={{textAlign: "center"}}>
+                        <TextField required label={this.state.passwordInputMessage || "Password"} placeholder="Password" type="password"
+                                   error={!!this.state.passwordInputMessage}
+                                   onChange={event => {
+                                       this.setState({
+                                           password: event.target.value,
+                                           lastUpdatedProperty: "password"
+                                       })
+                                   }}/>
+                    </div>
+                </Grid>
+                <Grid item xs={12}>
+                    <div style={{textAlign: "center"}}>
+                        <Button variant="contained" color="primary"
+                                onClick={this.signIn}>{this.state.signInStatus || "Log In"}</Button>
+                    </div>
+                </Grid>
+            </Grid>
+        )
+    }
+}
+
+class LoginViewProps implements RouteComponentProps {
+    history: History;
+    location: Location;
+    match: match<any>;
+    staticContext?: StaticContext;
+}
+
+class LoginViewState {
+    lastUpdatedProperty?: string;
+    email?: string;
+    emailInputMessage?: string;
+    password?: string;
+    passwordInputMessage?: string;
+    signInStatus?:string;
+}
