50329254adcaea9f755a86478cb9dbd07a9d5366
damien <damienmarble@gmail.com>
2018-09-26 01:43:44 -0400

    Removed unused import.

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import {BrowserRouter} from "react-router-dom";
-import {createStore, combineReducers} from "redux";
+import {createStore} from "redux";
 import {Provider} from "react-redux";
 import reducers from "./reducers";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = persistStore(createStore(persistReducer({storage, key: 'root'},reducers), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
 
 ReactDOM.render(
     <Provider store={store}>
