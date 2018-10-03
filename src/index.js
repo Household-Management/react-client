28117a4d417a72f8cf3003fc01bb9956dfe2dc9f
Damien Marble <damienmarble@gmail.com>
2018-10-03 00:33:41 +0000

    Added internationalization and database saving.

--- src/index.js
+++ src/index.js
@@ -4,22 +4,29 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import {BrowserRouter} from "react-router-dom";
-import {createStore} from "redux";
+import {createStore, applyMiddleware, compose} from "redux";
 import {Provider} from "react-redux";
 import reducers from "./reducers";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import { PersistGate } from 'redux-persist/integration/react'
 import hardSet from 'redux-persist/lib/stateReconciler/hardSet'
+import {IntlProvider} from "react-intl";
+import DatabasePersist from "./state/persistence/DatabasePersist";
 
-const store = createStore(persistReducer({storage, key: 'root', stateReconciler: hardSet},reducers), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(persistReducer({storage, key: 'root', stateReconciler: hardSet},reducers), 
+    composeEnhancers(applyMiddleware(DatabasePersist)));
 
 ReactDOM.render(
     <Provider store={store}>
-    <PersistGate loading={null} persistor={persistStore(store)}>
-        <BrowserRouter>
-            <App/>
-        </BrowserRouter>
+        <PersistGate loading={null} persistor={persistStore(store)}>
+            <IntlProvider>
+                <BrowserRouter>
+                    <App/>
+                </BrowserRouter>
+            </IntlProvider>
         </PersistGate>
     </Provider>
     , document.getElementById('root'));
