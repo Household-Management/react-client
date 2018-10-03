63b2c5d75d208afe40ae8e0a65b0223fede54066
Damien Marble <damienmarble@gmail.com>
2018-10-03 20:18:00 +0000

    Removed compiled file.

--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const auth_1 = require("./auth");
-const tasks_1 = require("./tasks");
-const redux_1 = require("redux");
-exports.default = redux_1.combineReducers({
-    auth: auth_1.default,
-    tasks: tasks_1.default
-});
