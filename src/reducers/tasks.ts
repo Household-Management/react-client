15a07a0642a22489a2f4f54aeadb6a7154c705db
Damien Marble <damienmarble@gmail.com>
2018-10-03 00:34:02 +0000

    New task saves to database.

--- src/reducers/tasks.ts
+++ src/reducers/tasks.ts
@@ -4,7 +4,6 @@ import NewTaskAction from "../actions/NewTaskAction";
 
 export default (state:TaskState, action:Action<any>) => {
   if (state === undefined && action.type !== "persist/REHYDRATE") {
-    console.log("Populate tasks", action.type);
     return new TaskState();
   }
   if (action.type === NewTaskAction.ACTION_TYPE) {
