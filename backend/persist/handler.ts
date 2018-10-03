aef1975f7049cbc14ec08a8e6219787042562b2e
Damien Marble <damienmarble@gmail.com>
2018-10-03 00:30:31 +0000

    Updated persist backend.

--- backend/persist/handler.ts
+++ backend/persist/handler.ts
@@ -1,9 +1,24 @@
 'use strict';
 import AWS = require("aws-sdk");
-import {AWSError} from "aws-sdk";
 
-export function saveData(event: any, context: any, callback: any) {
-    const dynamodb = new AWS.DynamoDB();
-    console.log("Called");
-    callback(event);
+export function saveData(data: any, context: any, callback: any) {
+    try {
+        const dynamodb = new AWS.DynamoDB();
+        let tableName;
+        switch (event.metadata.state) {
+            case "staging":
+                tableName = "Homeplanit-Users-Staging";
+                break;
+        }
+    dynamodb.putItem({
+        Item: AWS.DynamoDB.Converter.marshall(data),
+        TableName: tableName
+    }, function (error, data) {
+        callback(error, JSON.stringify({
+            statusCode: 200
+        }));
+    });
+    } catch(e){
+        callback(e);
+    }
 };
