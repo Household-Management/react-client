69e5d200b69147251244522c85eb8831f0ef66b6
Damien Marble <damienmarble@gmail.com>
2018-10-04 01:30:10 +0000

    Linting.

--- src/actions/RemoveTaskAction.ts
+++ src/actions/RemoveTaskAction.ts
@@ -6,7 +6,7 @@ export default class RemoveTaskAction implements Action<string> {
     public static ACTION_TYPE = "REMOVE_TASK";
     public type = RemoveTaskAction.ACTION_TYPE;
     public task: Task;
-    
+
     constructor(task: Task) {
         this.task = task;
     }
